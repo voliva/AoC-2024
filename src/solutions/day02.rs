3db640b114f29bbbafcbe52651edde67dd79466e
Victor Oliva <olivarra1@gmail.com>
2024-12-02 13:42:37 +0100

    cleanup day 2

--- src/solutions/day02.rs
+++ src/solutions/day02.rs
@@ -6,6 +6,22 @@ use std::io::{BufRead, BufReader};
 
 pub struct Problem;
 
+fn is_safe(line: &Vec<isize>) -> bool {
+    let diffs: Vec<_> = line
+        .into_iter()
+        .tuple_windows()
+        .map(|(a, b)| a - b)
+        .collect();
+
+    let positives = diffs.iter().filter(|v| **v > 0 && **v <= 3).count();
+    if positives == diffs.len() {
+        return true;
+    }
+
+    let negatives = diffs.iter().filter(|v| **v < 0 && **v >= -3).count();
+    return negatives == diffs.len();
+}
+
 impl Solver for Problem {
     type Input = Vec<Vec<isize>>;
     type Output1 = usize;
@@ -20,108 +36,28 @@ impl Solver for Problem {
     }
 
     fn solve_first(&self, input: &Self::Input) -> Result<Self::Output1, String> {
-        Ok(input
-            .iter()
-            .map(|v| {
-                if v.len() <= 1 {
-                    return 1;
-                };
-                let mut prev = v[0];
-                let is_increase = v[1] > v[0];
-                for k in &v[1..] {
-                    if (k - prev).abs() > 3 || *k == prev {
-                        return 0;
-                    }
-                    if is_increase && *k < prev {
-                        return 0;
-                    } else if !is_increase && *k > prev {
-                        return 0;
-                    }
-                    prev = *k;
-                }
-                return 1;
-            })
-            .sum())
+        Ok(input.iter().filter(|line| is_safe(*line)).count())
     }
 
     fn solve_second(&self, input: &Self::Input) -> Result<Self::Output2, String> {
         Ok(input
             .iter()
-            .map(|v| {
-                if v.len() <= 1 {
-                    return 1;
-                };
-
-                let diffs: Vec<_> = v.into_iter().tuple_windows().map(|(a, b)| a - b).collect();
-
-                let positives = diffs.iter().filter(|v| **v > 0 && **v <= 3).count();
-                let negatives = diffs.iter().filter(|v| **v < 0 && **v >= -3).count();
-
-                if positives == diffs.len() {
-                    return 1;
-                }
-                if positives >= diffs.len() - 2 {
-                    let (miss_idx, _) =
-                        diffs.iter().find_position(|v| **v <= 0 || **v > 3).unwrap();
-                    let positives_a = v
-                        .iter()
-                        .enumerate()
-                        .filter(|(i, _)| *i != miss_idx)
-                        .map(|(_, a)| a)
-                        .tuple_windows()
-                        .map(|(a, b)| a - b)
-                        .filter(|v| *v > 0 && *v <= 3)
-                        .count();
-                    let positives_b = v
-                        .iter()
-                        .enumerate()
-                        .filter(|(i, _)| *i != miss_idx + 1)
-                        .map(|(_, a)| a)
-                        .tuple_windows()
-                        .map(|(a, b)| a - b)
-                        .filter(|v| *v > 0 && *v <= 3)
-                        .count();
-                    if positives_a == diffs.len() - 1 || positives_b == diffs.len() - 1 {
-                        return 1;
-                    } else {
-                        return 0;
-                    }
+            .filter(|line| {
+                if is_safe(*line) {
+                    return true;
                 }
 
-                if negatives == diffs.len() {
-                    return 1;
-                }
-                if negatives >= diffs.len() - 2 {
-                    let (miss_idx, _) = diffs
-                        .iter()
-                        .find_position(|v| **v >= 0 || **v < -3)
-                        .unwrap();
-                    let tmp = v
-                        .iter()
-                        .enumerate()
-                        .filter(|(i, _)| *i != miss_idx)
-                        .map(|(_, a)| a)
-                        .tuple_windows()
-                        .map(|(a, b)| a - b)
-                        .collect_vec();
-                    let negatives_a = tmp.iter().filter(|v| **v < 0 && **v >= -3).count();
-                    let negatives_b = v
-                        .iter()
-                        .enumerate()
-                        .filter(|(i, _)| *i != miss_idx + 1)
-                        .map(|(_, a)| a)
-                        .tuple_windows()
-                        .map(|(a, b)| a - b)
-                        .filter(|v| *v < 0 && *v >= -3)
-                        .count();
-                    if negatives_a == diffs.len() - 1 || negatives_b == diffs.len() - 1 {
-                        return 1;
-                    } else {
-                        return 0;
-                    }
-                }
-                return 0;
+                return (0..line.len()).any(|idx| {
+                    is_safe(
+                        &line
+                            .iter()
+                            .enumerate()
+                            .filter(|(i, _)| *i != idx)
+                            .map(|(_, v)| *v)
+                            .collect(),
+                    )
+                });
             })
-            .sum())
+            .count())
     }
 }

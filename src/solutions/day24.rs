170311ee65d98332b8d057ed484526bece811a72
Victor Oliva <olivarra1@gmail.com>
2024-12-24 18:04:29 +0100

    remove dead code

--- src/solutions/day24.rs
+++ src/solutions/day24.rs
@@ -17,29 +17,6 @@ pub enum Operation {
     Hard(u8),
 }
 
-impl Operation {
-    fn to_str_rec(&self, tag: &str, others: &HashMap<String, Operation>) -> String {
-        match self {
-            Operation::Hard(_) => format!("{tag}"),
-            Operation::AND(l, r) => format!(
-                "({} & {})",
-                others.get(l).unwrap().to_str_rec(l, others),
-                others.get(r).unwrap().to_str_rec(r, others)
-            ),
-            Operation::OR(l, r) => format!(
-                "({} | {})",
-                others.get(l).unwrap().to_str_rec(l, others),
-                others.get(r).unwrap().to_str_rec(r, others)
-            ),
-            Operation::XOR(l, r) => format!(
-                "({} ^ {})",
-                others.get(l).unwrap().to_str_rec(l, others),
-                others.get(r).unwrap().to_str_rec(r, others)
-            ),
-        }
-    }
-}
-
 fn get_value(key: &str, ops: &HashMap<String, Operation>, values: &mut HashMap<String, u8>) -> u8 {
     if values.contains_key(key) {
         return *values.get(key).unwrap();
@@ -436,17 +413,6 @@ impl Solver for Problem {
     }
 
     fn solve_second(&self, input: &Self::Input) -> Result<Self::Output2, String> {
-        // let swaps = find_swaps(input, 4, &HashSet::new());
-        // return Ok(swaps.unwrap().iter().join(","));
-
-        // println!("{}", input.get("z00").unwrap().to_str_rec("z00", input));
-        // println!("{}", input.get("z01").unwrap().to_str_rec("z00", input));
-        // println!("{}", input.get("z02").unwrap().to_str_rec("z00", input));
-        // println!("{}", input.get("z03").unwrap().to_str_rec("z00", input));
-        // println!("{}", input.get("z04").unwrap().to_str_rec("z00", input));
-        // println!("{}", input.get("z05").unwrap().to_str_rec("z00", input));
-
-        // let mut prev_carry = None;
         let mut ops = input.clone();
         let mut fixed = HashSet::new();
         let mut swaps = HashSet::new();
@@ -463,113 +429,4 @@ impl Solver for Problem {
 
         todo!()
     }
-}
-
-fn count_bits(value: usize) -> usize {
-    let mut result = 0;
-    let mut value = value;
-    while value > 0 {
-        if value & 0x1 == 0 {
-            result += 1;
-        }
-        value = value >> 1;
-    }
-    result
-}
-
-fn find_swaps(
-    ops: &HashMap<String, Operation>,
-    swaps_left: usize,
-    swaps: &HashSet<String>,
-) -> Option<HashSet<String>> {
-    let mut values: HashMap<String, u8> = HashMap::new();
-    let (x, y, z) = (
-        get_full_value("x", &ops, &mut values),
-        get_full_value("y", &ops, &mut values),
-        get_full_value("z", &ops, &mut values),
-    );
-    let actual_sum = x + y;
-    let mut diff = z ^ actual_sum;
-    println!("{:0b}", diff);
-    todo!();
-
-    if diff == 0 {
-        return Some(HashSet::new());
-    } else if swaps_left == 0 {
-        return None;
-    }
-
-    let mut i = 0;
-    while (diff & 0x1) == 0 {
-        i += 1;
-        diff = diff >> 1;
-    }
-    let target = format!("z{:0>2}", i);
-    let mut visited = HashSet::new();
-    let gates = find_gates(&target, ops, &mut visited);
-    let correct_bit = (actual_sum >> i) & 0x1;
-    // let initial_bits = count_bits(diff);
-
-    for perm in gates.iter().permutations(2) {
-        if perm[0] == perm[1] {
-            continue;
-        }
-        if swaps_left >= 4 {
-            println!("Next {swaps_left} {:?}", perm);
-        }
-
-        let mut ops_copy = ops.clone();
-        let (a, b) = (
-            ops_copy.remove(perm[0]).unwrap(),
-            ops_copy.remove(perm[1]).unwrap(),
-        );
-        ops_copy.insert(perm[0].clone(), b);
-        ops_copy.insert(perm[1].clone(), a);
-        let mut swaps_copy = swaps.clone();
-        swaps_copy.insert(perm[0].clone());
-        swaps_copy.insert(perm[1].clone());
-
-        let mut values: HashMap<String, u8> = HashMap::new();
-        let new_value = get_full_value("z", &ops_copy, &mut values);
-        if (new_value >> i) & 0x1 != correct_bit {
-            // if count_bits(new_value ^ actual_sum) > initial_bits {
-            continue;
-        }
-
-        let result = find_swaps(&ops_copy, swaps_left - 1, &swaps_copy);
-        if result.is_some() {
-            let mut result = result.unwrap();
-            result.insert(perm[0].clone());
-            result.insert(perm[1].clone());
-            return Some(result);
-        }
-    }
-
-    None
-}
-
-fn find_gates(
-    start: &String,
-    ops: &HashMap<String, Operation>,
-    visited: &mut HashSet<String>,
-) -> HashSet<String> {
-    if visited.contains(start) {
-        return HashSet::new();
-    }
-    visited.insert(start.clone());
-
-    let top_gate = ops.get(start).unwrap();
-    let (l, r) = match top_gate {
-        Operation::AND(l, r) => (find_gates(l, ops, visited), find_gates(r, ops, visited)),
-        Operation::XOR(l, r) => (find_gates(l, ops, visited), find_gates(r, ops, visited)),
-        Operation::OR(l, r) => (find_gates(l, ops, visited), find_gates(r, ops, visited)),
-        _ => return HashSet::new(),
-    };
-
-    vec![start]
-        .into_iter()
-        .chain(l.iter())
-        .chain(r.iter())
-        .cloned()
-        .collect()
 }
